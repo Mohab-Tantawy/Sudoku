2357de8ec8d1ffc722ea3edea67c8455faaae182
MohabTantawy <es.mohab.khamis2023@alexu.edu.eg>
2025-11-28 19:50:09 +0200

    added the multi-thread method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,7 +17,7 @@ public class Main {
         System.out.println("=".repeat(50));
         System.out.println("1. Single Thread");
         System.out.println("2. Multi-Thread");
-        //System.out.println("3. Step-By-Step Thread");
+        //System.out.println("3. 27 Thread");
         System.out.print("Choose mode (1-3): ");
         int mode = scan.nextInt();
         scan.close();
@@ -30,15 +30,15 @@ public class Main {
                     runSingleThread(grid);
                     break;
                 case 2:
-                    //runMultiThread(grid);
+                    runMultiThread(grid);
                     break;
                 /*case 3:
-                    //runStepByStep(grid);
+                    //runUNKOWN(grid);
                     break;
                  */
                 default:
                     System.out.println("Invalid mode! Using Multi-Threaded mode. ");
-                    //runMultiThread(grid);
+                    runMultiThread(grid);
             }
         } else {
             System.out.println("ERROR: Couldn't Load Sudoku Grid :O ");
@@ -50,9 +50,12 @@ public class Main {
 
         Check check = new Check(grid);
 
-        int validRows = 0, validColumns = 0, validBoxes = 0;
+        validRows = 0;
+        validColumns = 0;
+        validBoxes = 0;
+
         System.out.println();
-            for (int i = 1; i < TOTAL_CHECKS; i++) {
+            for (int i = 1; i <= TOTAL_CHECKS; i++) {
                 if (check.checkRow(i)) {
                     validRows++;
                     System.out.println("Row " + i + ": Valid\n");
@@ -65,13 +68,108 @@ public class Main {
                 } else {
                     System.out.println("Column " + i + ": Invalid\n");
                 }
-                if (check.checkRow(i)) {
+                if (check.checkBox(i)) {
                     validBoxes++;
                     System.out.println("Box " + i + ": Valid\n");
                 } else {
                     System.out.println("Box " + i + ": Invalid\n");
                 }
                 System.out.println("_______________________");
+            }
+        }
+        private static void runMultiThread(int[][] grid){
+            System.out.println("\n Mode 2: MULTI-THREADED VALIDATION");
+            System.out.println("=".repeat(50));
+
+            Check check = new Check(grid);
+            validRows = 0;
+            validColumns = 0;
+            validBoxes = 0;
+
+            Thread rowThread = new Thread(new RowValidator(check));
+            Thread columnThread = new Thread(new ColumnValidator(check));
+            Thread boxThread = new Thread(new BoxValidator(check));
+
+            rowThread.start();
+            columnThread.start();
+            boxThread.start();
+
+            try{
+                rowThread.join();
+                columnThread.join();
+                boxThread.join();
+            }catch (InterruptedException e){
+                System.out.println("Thread Execution is Interrupted " + e.getMessage());
+                return;
+            }
+        }
+        static class RowValidator implements Runnable{
+            private Check check;
+
+            public RowValidator(Check check){
+                this.check = check;
+            }
+
+            @Override
+            public void run(){
+                System.out.println("Row validation thread started...");
+                for(int i = 1; i <= TOTAL_CHECKS; i++){
+                    if(check.checkRow(i)){
+                        synchronized (Main.class) {
+                            validRows++;
+                        }
+                        System.out.println("Row " + i + ": Valid");
+                    }else{
+                        System.out.println("Row " + i + ": Invalid");
+                    }
+                }
+                System.out.println("Row Validation thread completed.");
+            }
+        }
+        static class ColumnValidator implements Runnable{
+            private Check check;
+
+            public ColumnValidator(Check check){
+                this.check = check;
+            }
+
+            @Override
+            public void run(){
+                System.out.println("Column validation thread started...");
+                for(int i = 1; i <= TOTAL_CHECKS; i++){
+                    if(check.checkColumn(i)){
+                        synchronized (Main.class) {
+                            validColumns++;
+                        }
+                        System.out.println("Column " + i + ": Valid");
+                    }else{
+                        System.out.println("Column " + i + ": Invalid");
+                    }
+                }
+                System.out.println("Column Validation thread completed.");
+            }
+        }
+        static class BoxValidator implements Runnable{
+            private Check check;
+
+            public BoxValidator(Check check){
+                this.check = check;
+            }
+
+            @Override
+            public void run(){
+                System.out.println("Box validation thread started...");
+                for(int i = 1; i <= TOTAL_CHECKS; i++){
+                    if(check.checkBox(i)){
+                        synchronized (Main.class) {
+                            validBoxes++;
+                        }
+                        System.out.println("Box " + i + ": Valid");
+                    }else{
+                        System.out.println("Box " + i + ": Invalid");
+                    }
+                }
+                System.out.println("Box Validation thread completed.");
             }
         }
 }
