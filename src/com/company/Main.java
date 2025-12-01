b0996fc62abf7c11673fea80e020d06e7f79a239
MohabTantawy <es.mohab.khamis2023@alexu.edu.eg>
2025-12-01 02:47:55 +0200

    Added the diagrams (sequence made by Youssef Elwishy) and merged manually with the bonus part

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,41 +5,54 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        if(args.length == 1 && ("-h".equals(args[0]) || "--help".equals(args[0]))){
+            printUsage();
+            return;
+        }
 
-        System.out.println("🎯 SUDOKU VALIDATOR - MULTI-MODE");
-        System.out.println("=".repeat(50));
-        System.out.println("1. Single Thread");
-        System.out.println("2. Multi-Thread");
-        System.out.println("3. Ultra Multi-Thread");
-        System.out.print("Choose mode (1-3): ");
-        int mode = scan.nextInt();
-        scan.close();
+        if(args.length != 2){
+            System.out.println("ERROR: Incorrect number of arguments");
+            printUsage();
+            System.exit(1);
+        }
+        String filename = args[0];
+        int mode = 0;
+        try{
+            mode = Integer.parseInt(args[1]);
+        }catch (NumberFormatException e){
+            System.out.println("ERROR: Mode must be a number (1 or 2 or 3)");
+            printUsage();
+            System.exit(1);
+        }
 
-        int[][] grid = Grid.getGrid("sudoku_test.csv");
+        if(mode < 1 || mode >3){
+            System.out.println("ERROR: Mode must be 1 or 2 or 3");
+        }
 
-        if (grid[0][0] != 0) {
-            SudokuValidator validator;
-            switch (mode) {
-                case 1:
-                    validator = new runSingleThread();
-                    break;
-                case 2:
-                    validator = new runMultiThread();
-                    break;
-                case 3:
-                    validator = new runUltraMultiThread();
-                    break;
+        int[][] grid = Grid.getGrid(filename);
+        if(grid[0][0] == 0){
+            System.out.println("ERROR: Could not load Sudoku grid from: " + filename);
+            System.out.println("Please check if the file exists and has correct format");
+            System.exit(1);
+        }
+        SudokuValidator validator = createValidator(mode);
+        validator.validate(grid);
+    }
+    private static SudokuValidator createValidator(int mode){
+        switch (mode) {
+            case 1:
+                return new runSingleThread();
+            case 2:
+                return new runMultiThread();
+            case 3:
+                return new runUltraMultiThread();
 
-                default:
-                    System.out.println("Invalid mode! Using Multi-Threaded mode. ");
-                    validator = new runMultiThread();
-                    break;
-            }
-            validator.validate(grid);
-        } else {
-            System.out.println("ERROR: Couldn't Load Sudoku Grid :O ");
+            default:
+                System.out.println("Invalid mode! Using Multi-Threaded mode. ");
+                return new runMultiThread();
         }
     }
-}
+    private static void printUsage(){
 
+    }
+}
